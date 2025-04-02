dcf2be40a0f74dcf416bfb1a54bde46f21462ecb
Suttiago <150452828+Suttiago@users.noreply.github.com>
2025-04-02 12:42:36 -0300

    Create operadores_dowload.py

---
+++ SQL/scripts/operadores_dowload.py
@@ -0,0 +1,39 @@
+import os
+from bs4 import BeautifulSoup
+import requests
+
+url = 'https://dadosabertos.ans.gov.br/FTP/PDA/operadoras_de_plano_de_saude_ativas/'
+
+response = requests.get(url)
+
+def listar_csv():
+    soup = BeautifulSoup(response.text, "html.parser")
+    csv = []
+    
+    for link in soup.find_all('a', href=True):  
+        if link['href'].endswith('.csv'):
+            csv.append(url + link['href']) 
+    return csv
+
+
+def baixar_csv(csv):
+    destino = 'dados_operadoras/dados_csv'
+
+    if not os.path.exists(destino):
+        os.makedirs(destino)
+
+    for csvlink in csv:
+        nome_arquivo = os.path.join(destino, os.path.basename(csvlink))
+        
+        response = requests.get(csvlink)
+        if response.status_code == 200:
+            with open(nome_arquivo, "wb") as f:
+                f.write(response.content)
+            print(f"Arquivo salvo: {nome_arquivo}")
+        else:
+            print(f"Falha ao baixar: {csvlink}")
+
+
+if __name__ == "__main__":
+    csv_links = listar_csv()
+    baixar_csv(csv_links)
